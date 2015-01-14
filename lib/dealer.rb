8c3a4f6dbcf52a6bc84b273b496b881512b4326a
Duane M. Moody <dmmoody@gmail.com>
2015-01-14 10:10:03 -0800

    add vehicle make method, save method

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -1,1 +1,17 @@
-  
+  class Vehicle
+    @@vehicle = []
+
+    define_method(:initialize) do |make, model, year|
+      @make = make
+      @model = model
+      @year = year
+    end
+
+    define_method(:save) do
+      @@vehicle.push(self)
+    end
+
+    define_method(:make) do
+      @make
+    end
+end
